3c3da8982cc676f1ef6d03efd5216aa2f067fc91
Lin Cheng Kai <whitefur@domiso.cc>
2013-07-18 16:34:17 +0800

    Initial Commit

---
+++ StickerPlistGenerator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  StickerPlistGenerator
+//
+//  Created by Lin Cheng Kai on 13/7/18.
+//  Copyright (c) 2013年 Lin Cheng Kai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
